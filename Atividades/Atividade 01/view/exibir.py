508261c7a6864e1eff656d44b06348a6084c158d
Pedro Henrique de B. Canabarro <111590901+pedrocanabarroo@users.noreply.github.com>
2026-08-10 09:59:22 -0300

    Create exibir.py

---
+++ Atividades/Atividade 01/view/exibir.py
@@ -0,0 +1,6 @@
+class Visualizar:
+    def mostrar_parcial(self, indice, soma):
+        print(f"Thread {indice} -> soma parcial: {soma}")   
+
+    def mostrar_total(self, total):
+        print(f"Soma total final: {total}")
